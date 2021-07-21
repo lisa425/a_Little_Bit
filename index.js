4122c843c3043a56acd6c79604355401fcff92f8
chaewon <lisa9902@naver.com>
2021-07-21 12:20:22 +0900

    user info input(signup)

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const { User } = require("./models/User");
+const bodyParser = require("body-parser");
+
+//client에서 오는 정보를 bodyParser로 분석해서 가져온다.
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://chaewon:thtldmlznzl425@alittlebit.oxgxw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
@@ -9,5 +17,14 @@ mongoose.connect('mongodb+srv://chaewon:thtldmlznzl425@alittlebit.oxgxw.mongodb.
 
 
 app.get('/',(req,res)=>res.send('Hello World!'))
+
+app.post('/test',(req,res)=>{
+    //test 유저 정보를 client에서 가져와 DB에 저장
+    const user = new User(req.body);
+    user.save((err,doc)=>{
+        if(err) return res.json({ success:false, err })
+        return res.status(200).json({ success:true })
+    })
+})
 
 app.listen(port,() => console.log(`Example app listening on port ${port}`));
