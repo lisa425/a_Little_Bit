e86f07ad84768aca51d5528abb710b9c25729d4b
chaewon <lisa425@sookmyung@ac.kr>
2021-08-03 14:19:28 +0900

    fix:draggable 임시방편

--- client/src/components/views/TestPage/TestEarthList.js
+++ client/src/components/views/TestPage/TestEarthList.js
@@ -3,6 +3,7 @@ import Axios from 'axios';
 import '../../../css/TestPage/TestEarthList.css';
 import EarthImg from '../../../assets/images/earth.png';
 import Draggable from "react-draggable";
+//import {Draggable,useDraggabe} from 'use-draggable';
 
 const TestEarthList = () => {
 
@@ -31,73 +32,29 @@ const TestEarthList = () => {
     // const closeModal = () => {
     //     setModalOpen(false);
     // }
-    
-    //지구 draggable position 설정
-    // const [earthPosition, setEarthPosition] = useState({id:0,x:0,y:0});
-    // const trackPosition = (event,data) => {
-    //     let targetEarth = document.querySelector('#'+id);
-    //     setEarthPosition({id:data.id, x: data.x, y: data.y });
-    //     targetEarth.positon.x = 
-    // }
-    // function useDraggable(el){
-    //     const [{ x, y },setOffset] = useState({x:0,y:0});
+    const targetEarth = useRef();
 
-    //     useEffect(()=>{
-    //         const handleMouseDown = event => {
-    //             const startX = event.pageX - x;
-    //             const startY = event.pageY - y;
-
-    //             const handelMouseMove = event =>{
-    //                 const newX = event.pageX - startX;
-    //                 const newY = event.pageY - startY;
-    //                 setOffset({x:newX,y:newY});
-    //             };
-
-    //             document.addEventListener("mousemove",handelMouseMove);
-
-    //             document.addEventListener("mouseup",()=>{
-    //                 document.removeEventListener("mousemove",handelMouseMove);
-    //             },{ once:true }
-    //             );
-    //         };
-    //         el.current.addEventListener("mousedown",handleMouseDown);
-
-    //         return()=>{
-    //             el.current.removeEventListener("mousedown",handleMouseDown);
-    //         };
-    //     },[x,y]);
-        
-    //     useEffect(()=>{
-    //         el.current.style.transform = `translate(${x}px,${y}px)`;
-    //     },[x,y]);
-    // }
-
-    // const DraggableEarth = ({ children }) => {
-    //     const earthRef = useRef(null);
-    //     useDraggable(earthRef);
-
-    //     return(
-    //         <div className="earth" ref={earthRef}>
-    //             {children}
-    //         </div>
-    //     )
-    // }
     const renderEarth = Earth.map((earth,index) => {
-
         // 지구를 랜덤한 위치에 출력한다. 랜덤한 position 위치 값 설정
         let min = Math.ceil(-10);
-        let max = Math.floor(300);
-        let valueBottom = Math.floor(Math.random()*(max-min)*15);
-        let valueLeft = Math.floor(Math.random()*(max-min));
+        let max = Math.floor(3000);
+        let valueBottom = Math.floor(Math.random()*(max-min)*2);
+        let valueLeft = Math.floor(Math.random()*(max-min)/2);
         let valueTop = Math.floor(Math.random()*(max-min));
         //style={{left:valueLeft, bottom: valueBottom, top:valueTop}}
 
-
         return(
             <Draggable>
-                <div id={index+1} className="earth-list" style={{left:valueLeft, bottom: valueBottom, top:valueTop}}>
+                <div 
+                    ref={targetEarth}
+                    id={index+1} 
+                    className="earth-list"
+                    style={{left:valueLeft, top:valueBottom}}
+                >
                     <div className="earth-content" key={index+1}>
-                        <a href={`/test/earthlist/${earth._id}`}><img src={EarthImg} alt="earth image"/></a>
+                        {/* <a href={`/test/earthlist/${earth._id}`}> */}
+                            <img src={EarthImg} alt="earth image"/>
+                        
                         <p className="earth-index">{index+1}</p>
 
                     </div>
@@ -105,7 +62,6 @@ const TestEarthList = () => {
             </Draggable>
         )
     })
-
 
     return(
         <main className="TestEarthList">
