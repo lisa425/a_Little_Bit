2a22747a0541b6d62a3e739dfd3c7b833e57d331
chaewon <lisa425@sookmyung@ac.kr>
2021-08-09 01:50:34 +0900

    feat:cardnews page 생성

--- client/src/components/views/CardNewsPage/CardNewsPage.js
+++ client/src/components/views/CardNewsPage/CardNewsPage.js
@@ -1,10 +1,31 @@
 import React from 'react';
-
+import '../../../css/CardNewsPage/CardNewsPage.css'
+import {ReactComponent as CardNewsPixel} from '../../../assets/images/cardNews/cardnews_pixel.svg';
+import CardNewsPixelPng from '../../../assets/images/cardNews/cardnews_pixel.png';
 const CardNewsPage = () => {
+
+    //map으로 카드뉴스 넣기
+    const cardNewsList = {
+        "netflix":"url"
+    }
+    const cardNewsContents = () => {
+        return(
+            <div className="card-img" >
+                <img src="" alt="thumbnail"/>
+                HIHIHIHI!!!
+            </div>
+        )
+    }
     return(
-        <div>
-            HI
-        </div>
+        <main className="cardNews">
+            <h2>Fun Facts</h2>
+            <p>함께 실천할 액션 플랜을 선택해, 나의 선택이 만들어내는 긍정적인 영향을 확인해보세요.</p>
+            <div className="cardnews-container">
+                {cardNewsContents}
+            </div>
+            <img src={CardNewsPixelPng} className="card-news-pixel"/>
+
+        </main>
     )
 }
 
