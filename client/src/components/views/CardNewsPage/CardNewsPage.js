c12ddf2e0c96bbadb806d6b450aacefe8c4d82cc
chaewon <lisa425@sookmyung@ac.kr>
2021-08-10 15:49:41 +0900

    style:cardnews page style(layout)

--- client/src/components/views/CardNewsPage/CardNewsPage.js
+++ client/src/components/views/CardNewsPage/CardNewsPage.js
@@ -1,29 +1,27 @@
 import React from 'react';
 import '../../../css/CardNewsPage/CardNewsPage.css'
 import {ReactComponent as CardNewsPixel} from '../../../assets/images/cardNews/cardnews_pixel.svg';
-import CardNewsPixelPng from '../../../assets/images/cardNews/cardnews_pixel.png';
+import earth from '../../../assets/images/earth.png'
 const CardNewsPage = () => {
 
     //map으로 카드뉴스 넣기
-    const cardNewsList = {
-        "netflix":"url"
-    }
-    const cardNewsContents = () => {
-        return(
-            <div className="card-img" >
-                <img src="" alt="thumbnail"/>
-                HIHIHIHI!!!
-            </div>
-        )
-    }
     return(
         <main className="cardNews">
             <h2>Fun Facts</h2>
             <p>함께 실천할 액션 플랜을 선택해, 나의 선택이 만들어내는 긍정적인 영향을 확인해보세요.</p>
             <div className="cardnews-container">
-                {cardNewsContents}
+                <div className="cardnews-item"><img src={earth}/></div>
+                <div className="cardnews-item"><img src={earth}/></div>
+                <div className="cardnews-item"><img src={earth}/></div>
+                <div className="cardnews-item"><img src={earth}/></div>
+                <div className="cardnews-item"><img src={earth}/></div>
+                <div className="cardnews-item"><img src={earth}/></div>
+                <div className="cardnews-item"><img src={earth}/></div>
+                <div className="cardnews-item"><img src={earth}/></div>
+                <div className="cardnews-item"><img src={earth}/></div>
+                <div className="cardnews-item"><img src={earth}/></div>
             </div>
-            <img src={CardNewsPixelPng} className="card-news-pixel"/>
+            <CardNewsPixel className="card-news-pixel"/>
 
         </main>
     )
