ce324d6a31f3059495656fc8d0666c788e646350
chaewon <lisa425@sookmyung@ac.kr>
2021-08-17 14:58:17 +0900

    feat:actionplan page pixel interaction

--- client/src/components/views/ActionPlanPage/ActionPlanPage.js
+++ client/src/components/views/ActionPlanPage/ActionPlanPage.js
@@ -1,156 +1,184 @@
 import React,{useState,useRef} from 'react';
 import '../../../css/ActionPlanPage/ActionPlanPage.css';
+//background img
 import PolarBear from '../../../assets/images/actionplans/bear_img.png';
 import Turtle from '../../../assets/images/actionplans/turtle_img.png';
 import Panda from '../../../assets/images/actionplans/panda_img.png';
+//next button img
 import { ReactComponent as Next } from '../../../assets/images/next.svg';
-import { ReactComponent as Before } from '../../../assets/images/before.svg';
+//action list icon
 import { ReactComponent as Cloud } from '../../../assets/images/actionplans/actionplanicon/cloud.svg';
 import { ReactComponent as Kakaotalk } from '../../../assets/images/actionplans/actionplanicon/kakaotalk.svg';
 import { ReactComponent as Email } from '../../../assets/images/actionplans/actionplanicon/email.svg';
 import { ReactComponent as SNS } from '../../../assets/images/actionplans/actionplanicon/instagram.svg';
 import { ReactComponent as Netflix} from '../../../assets/images/actionplans/actionplanicon/netflix.svg';
 import { ReactComponent as Zoom } from '../../../assets/images/actionplans/actionplanicon/zoom.svg';
-import { ReactComponent as ActionPlanPixel } from '../../../assets/images/actionplans/ActionPlan_pixel2.svg';
+import { ReactComponent as Brightness } from '../../../assets/images/actionplans/actionplanicon/brightness.svg';
+import { ReactComponent as Darkmode } from '../../../assets/images/actionplans/actionplanicon/darkmode.svg';
+import { ReactComponent as Energymode } from '../../../assets/images/actionplans/actionplanicon/energymode.svg';
+import { ReactComponent as Smartphone} from '../../../assets/images/actionplans/actionplanicon/smartphone.svg';
+import { ReactComponent as Wifi } from '../../../assets/images/actionplans/actionplanicon/wifi.svg';
+//action plan pixel
+import { ReactComponent as ZoomPixel } from '../../../assets/images/actionplans//actionplanpixel/zoompixel.svg';
+import { ReactComponent as BrightnessPixel } from '../../../assets/images/actionplans//actionplanpixel/brightness.svg';
+import { ReactComponent as CloudPixel } from '../../../assets/images/actionplans//actionplanpixel/cloud.svg';
+import { ReactComponent as DarkmodePixel } from '../../../assets/images/actionplans//actionplanpixel/darkmode.svg';
+import { ReactComponent as EmailfilePixel } from '../../../assets/images/actionplans//actionplanpixel/emailfile.svg';
+import { ReactComponent as EmaillistPixel } from '../../../assets/images/actionplans//actionplanpixel/emaillist.svg';
+import { ReactComponent as EnergymodePixel } from '../../../assets/images/actionplans//actionplanpixel/energymode.svg';
+import { ReactComponent as MessengerPixel } from '../../../assets/images/actionplans//actionplanpixel/messenger.svg';
+import { ReactComponent as NetflixPixel } from '../../../assets/images/actionplans//actionplanpixel/netflix.svg';
+import { ReactComponent as SmartphonePixel } from '../../../assets/images/actionplans//actionplanpixel/smartphone.svg';
+import { ReactComponent as SnsPixel } from '../../../assets/images/actionplans//actionplanpixel/sns.svg';
+import { ReactComponent as WifiPixel } from '../../../assets/images/actionplans//actionplanpixel/wifi.svg';
 
 const ActionPlanPage = () => {
-    //그리드 갯수만큼 배열 생성 
-    const ROW_LENGTH = 37;
-    const COLUMN_LENGTH = 65;
-    let actionPlanArray = [];
-    let existPositionArray = [];
-
-    const checkExist = (row,col) => {
-        existPositionArray.find(object => {
-            if(object.column === col & object.row === row){
-                return true;
-            }else{
-                return false;
-            }
-        })
-    }
-
-    for(let i=0; i<(ROW_LENGTH*COLUMN_LENGTH); i++){
-        actionPlanArray.push(i);
-        // let randomPositionColumn = Math.floor(Math.random()*COLUMN_LENGTH+1);
-        // let randomPositionRow = Math.floor(Math.random()*ROW_LENGTH+1);
-        // existPositionArray.push({
-        //     'column':randomPositionColumn,
-        //     'row':randomPositionRow
-        // })
-        // if(!checkExist(randomPositionRow,randomPositionColumn)){
-        //     existPositionArray.push({
-        //         'column':randomPositionColumn,
-        //         'row':randomPositionRow
-        //     })
-        //     console.log('push complete!')
-        // }
-    }
-    const renderPixel = actionPlanArray.map((position,index) => {
-        let classListArray = ['fill-pixel','dot-pixel','mosaic-pixel']
-        
-        let random_class = Math.floor(Math.random()*3);
-
-        let classname = classListArray[random_class];
-        // if(index>1000 && position.row>6){
-        //     classname = "fill-pixel";
-        // }else if(index<700 && position.row<5){
-        //     classname = "dot-pixel";
-        // }else{
-        //     classname = "mosaic-pixel";
-        // }
-        return (
-            <div className={`${classname} 'item-${index}'`}></div>
-        )
-    })
-
-    
-
-    // const netflix_pixel=document.getElementById('#netflix');
-    // const messenger_pixel =  document.getElementById('#messenger');
-    // const brightness_pixel =  document.getElementById('#brightness');
-    // const emaillist_pixel =  document.getElementById('#emaillist');
-    // const sns_pixel =  document.getElementById('#sns');
-    // const smartphone_pixel=  document.getElementById('#smartphone');
-    // const energymode_pixel =  document.getElementById('#energymode');
-    // const wifi_pixel =  document.getElementById('#wifi');
-    // const zoom_pixel =  document.getElementById('#zoom');
-    // const darkmode_pixel =  document.getElementById('#darkmode');
-    // const emailfile_pixel =  document.getElementById('#emailfile');
-    // const cloud_pixel =  document.getElementById('#cloud');
-
-    const actionplan_pixel = useRef();
+    const zoom_pixel = useRef();
+    const brightness_pixel = useRef();
+    const cloud_pixel = useRef();
+    const darkmode_pixel = useRef();
+    const emailfile_pixel = useRef();
+    const emaillist_pixel = useRef();
+    const energymode_pixel = useRef();
+    const messenger_pixel = useRef();
+    const netflix_pixel = useRef();
+    const smartphone_pixel = useRef();
+    const sns_pixel = useRef();
+    const wifi_pixel = useRef();
 
     const [VideoAction,setVideoAction] = useState(false);
     const onVideoActionHandler = (event) => {
-        setVideoAction(!VideoAction);
-        // const netflix_pixel = actionplan_pixel.current.getElementById('netflix');
-        // console.log(netflix_pixel)
-        // if(VideoAction){ 
-        //     console.log(VideoAction)
-           
-        //     netflix_pixel.style.display="none"
-        // }else{
-        //     console.log(VideoAction)
-        //     netflix_pixel.style.display="block" 
-        // }
+        if(VideoAction){ 
+            netflix_pixel.current.style.animation="pixelShow 0.2s forwards" 
+            setVideoAction(!VideoAction);
+        }else{
+            netflix_pixel.current.style.animation="pixelHide 0.2s forwards"
+            setVideoAction(!VideoAction);
+        }
     }
 
     const [SnsAction,setSnsAction] = useState(false);
     const onSnsActionHandler = (event) => {
-        setSnsAction(!SnsAction);
+        if(SnsAction){ 
+            sns_pixel.current.style.animation="pixelShow 0.2s forwards" 
+            setSnsAction(!SnsAction);
+        }else{
+            sns_pixel.current.style.animation="pixelHide 0.2s forwards"
+            setSnsAction(!SnsAction);
+        }
     }
 
     const [CloudAction,setCloudAction] = useState(false);
     const onCloudActionHandler = (event) => {
-        setCloudAction(!CloudAction);
+        if(CloudAction){ 
+            cloud_pixel.current.style.animation="pixelShow 0.2s forwards" 
+            setCloudAction(!CloudAction);
+        }else{
+            cloud_pixel.current.style.animation="pixelHide 0.2s forwards"
+            setCloudAction(!CloudAction);
+        }
     }
 
     const [MessengerAction,setMessengerAction] = useState(false);
     const onMessangerActionHandler = (event) => {
-        setMessengerAction(!MessengerAction);
+        if(MessengerAction){ 
+            messenger_pixel.current.style.animation="pixelShow 0.2s forwards" 
+            setMessengerAction(!MessengerAction);
+        }else{
+            messenger_pixel.current.style.animation="pixelHide 0.2s forwards"
+            setMessengerAction(!MessengerAction);
+        }
     }
 
     const [ZoomAction,setZoomAction] = useState(false);
     const onZoomActionHandler = (event) => {
-        setZoomAction(!ZoomAction);
+        if(ZoomAction){ 
+            zoom_pixel.current.style.animation="pixelShow 0.2s forwards" 
+            setZoomAction(!ZoomAction);
+        }else{
+            zoom_pixel.current.style.animation="pixelHide 0.2s forwards"
+            setZoomAction(!ZoomAction);
+        }
     }
 
     const [EmailListAction,setEmailListAction] = useState(false);
     const onEmailListActionHandler = (event) => {
-        setEmailListAction(!EmailListAction);
+        if(EmailListAction){ 
+            emaillist_pixel.current.style.animation="pixelShow 0.2s forwards" 
+            setEmailListAction(!EmailListAction);
+        }else{
+            emaillist_pixel.current.style.animation="pixelHide 0.2s forwards"
+            setEmailListAction(!EmailListAction);
+        }
     }
 
     const [WifiAction,setWifiAction] = useState(false);
     const onWifiActionHandler = (event) => {
-        setWifiAction(!WifiAction);
+        if(WifiAction){ 
+            wifi_pixel.current.style.animation="pixelShow 0.2s forwards" 
+            setWifiAction(!WifiAction);
+        }else{
+            wifi_pixel.current.style.animation="pixelHide 0.2s forwards"
+            setWifiAction(!WifiAction);
+        }
     }
 
     const [DarkModeAction,setDarkModeAction] = useState(false);
     const onDarkModeActionHandler = (event) => {
-        setDarkModeAction(!DarkModeAction);
+        if(DarkModeAction){ 
+            darkmode_pixel.current.style.animation="pixelShow 0.2s forwards" 
+            setDarkModeAction(!DarkModeAction);
+        }else{
+            darkmode_pixel.current.style.animation="pixelHide 0.2s forwards"
+            setDarkModeAction(!DarkModeAction);
+        }
     }
 
     const [EnergyModeAction,setEnergyModeAction] = useState(false);
     const onEnergyModeActionHandler = (event) => {
-        setEnergyModeAction(!EnergyModeAction);
+        if(EnergyModeAction){ 
+            energymode_pixel.current.style.animation="pixelShow 0.2s forwards" 
+            setEnergyModeAction(!EnergyModeAction);
+        }else{
+            energymode_pixel.current.style.animation="pixelHide 0.2s forwards" 
+            setEnergyModeAction(!EnergyModeAction);
+        }
     }
 
     const [BrightnessAction,setBrightnessAction] = useState(false);
     const onBrightnessActionHandler = (event) => {
-        setBrightnessAction(!BrightnessAction);
+        if(BrightnessAction){ 
+            brightness_pixel.current.style.animation="pixelShow 0.2s forwards" 
+            setBrightnessAction(!BrightnessAction);
+        }else{
+            brightness_pixel.current.style.animation="pixelHide 0.2s forwards" 
+            setBrightnessAction(!BrightnessAction);
+        }
     }
 
     const [SmartphoneAction,setSmartphoneAction] = useState(false);
     const onSmartphoneActionHandler = (event) => {
-        setSmartphoneAction(!SmartphoneAction);
+        if(SmartphoneAction){ 
+            smartphone_pixel.current.style.animation="pixelShow 0.2s forwards" 
+            setSmartphoneAction(!SmartphoneAction);
+        }else{
+            smartphone_pixel.current.style.animation="pixelHide 0.2s forwards" 
+            setSmartphoneAction(!SmartphoneAction);
+        }
     }
 
     const [EmailFileAction,setEmailFileAction] = useState(false);
     const onEmailFileActionHandler = (event) => {
-        setEmailFileAction(!EmailFileAction);
+        if(EmailFileAction){ 
+            emailfile_pixel.current.style.animation="pixelShow 0.2s forwards" 
+            setEmailFileAction(!EmailFileAction);
+        }else{
+            emailfile_pixel.current.style.animation="pixelHide 0.2s forwards" 
+            setEmailFileAction(!EmailFileAction);
+        }
     }
 
+    //배경이미지 조작
     const backImgObject = {1:PolarBear,2:Turtle,3:Panda}
     const [background,setBackground] = useState(1);
     const [backImg,setBackImg] = useState(backImgObject[background]);
@@ -158,7 +186,6 @@ const ActionPlanPage = () => {
         if(background==3){
             setBackImg(backImgObject[1]);
             setBackground(1);
-
         }else{
             setBackImg(backImgObject[background+1]);
             setBackground(background+1);
@@ -171,15 +198,25 @@ const ActionPlanPage = () => {
                 <img src={backImg} alt="북극곰(polar bear image)"/>
             </div>
             <div id="actionplan-pixel-container">
-                <ActionPlanPixel ref={actionplan_pixel}/> 
-                {/* {renderPixel} */}
-            </div>
-            <div className="background-img-handler">
-                <button className="image-handler" onClick={onNextBackgroundHandler}><Next/></button>
+                <ZoomPixel ref={zoom_pixel}/>
+                <BrightnessPixel ref={brightness_pixel}/>
+                <CloudPixel ref={cloud_pixel}/>
+                <DarkmodePixel ref={darkmode_pixel}/>
+                <EmailfilePixel ref={emailfile_pixel}/>
+                <EmaillistPixel ref={emaillist_pixel}/>
+                <EnergymodePixel ref={energymode_pixel}/>
+                <MessengerPixel ref={messenger_pixel}/>
+                <NetflixPixel ref={netflix_pixel}/>
+                <SmartphonePixel ref={smartphone_pixel}/>
+                <SnsPixel ref={sns_pixel}/>
+                <WifiPixel ref={wifi_pixel}/>
             </div>
             <article className="actionplan-contents">
                 <h2>Action Plan</h2>
                 <p>함께 실천할 액션 플랜을 선택해, 나의 선택이 만들어내는 긍정적인 영향을 확인해보세요.</p>
+                
+                <button className="image-handler" onClick={onNextBackgroundHandler}><Next/></button>
+                
                 <div className="actionplan-list-container">
                     <ul className="actionplan-list">
                         <li className={VideoAction?"actionplan-item-active":"actionplan-item"} onClick={onVideoActionHandler}>
@@ -226,35 +263,35 @@ const ActionPlanPage = () => {
                         </li>
                         <li className={WifiAction?"actionplan-item-active":"actionplan-item"} onClick={onWifiActionHandler}>
                             <div className="item-wrapper">
-                                <Email />
+                                <Wifi />
                                 <span>데이터 대신 Wi-Fi 사용</span>
                             </div>
                             <hr/>
                         </li>
                         <li className={DarkModeAction?"actionplan-item-active":"actionplan-item"} onClick={onDarkModeActionHandler}>
                             <div className="item-wrapper">
-                                <Email />
+                                <Darkmode />
                                 <span>다크 모드 설정</span>
                             </div>
                             <hr/>
                         </li>
                         <li className={EnergyModeAction?"actionplan-item-active":"actionplan-item"} onClick={onEnergyModeActionHandler}>
                             <div className="item-wrapper">
-                                <Email />
+                                <Energymode />
                                 <span>에너지 절약 모드 설정</span>
                             </div>
                             <hr/>
                         </li>
                         <li className={BrightnessAction?"actionplan-item-active":"actionplan-item"} onClick={onBrightnessActionHandler}>
                             <div className="item-wrapper">
-                                <Email />
+                                <Brightness />
                                 <span>화면 밝기 낮추기</span>
                             </div>
                             <hr/>
                         </li>
                         <li className={SmartphoneAction?"actionplan-item-active":"actionplan-item"} onClick={onSmartphoneActionHandler}>
                             <div className="item-wrapper">
-                                <Email />
+                                <Smartphone />
                                 <span>스마트폰 확인 빈도 수 줄이기</span>
                             </div>
                             <hr/>
