64babba8bef6829887edd9fe5b2eeb8664ecc971
Im Chaewon <lisa425@sookmyung.ac.kr>
2021-08-26 12:43:41 +0900

    dev environment issue close

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = 5000;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const mongoose = require('mongoose')
+const config = require('./config/key')
 
 //client에서 오는 정보를 bodyParser로 분석해서 가져온다.
 //application/x-www-form-urlencoded
@@ -12,18 +13,16 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-mongoose.connect('mongodb+srv://chaewon:thtldmlznzl425@alittlebit.oxgxw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
-    useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false
-}).then(()=>console.log('mongoDB is connected')).catch(err=>console.log(err))
+mongoose.connect(config.mongoURI,{
+    useNewUrlParser:true, useUnifiedTopology:true, 
+    useCreateIndex:true, useFindAndModify:false
+})
+.then(()=>console.log('mongoDB is connected'))
+.catch(err=>console.log(err))
 
 
-app.get('/',(req,res)=>res.send('Hello World!'))
 app.use('/api/test', require('./routes/test'));
 
 
 
 app.listen(port,() => console.log(`Example app listening on port ${port}`));
-
-//https://wellbell.tistory.com/43 
-//commit test...
-//commit test 2..
