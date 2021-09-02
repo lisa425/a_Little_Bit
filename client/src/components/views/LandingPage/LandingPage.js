0e7ba7cc24771576c4c7d8574d144af1344695f2
Im Chaewon <lisa425@sookmyung.ac.kr>
2021-09-02 15:18:26 +0900

    fix:landing page logo change

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,15 +1,15 @@
-import React,{ useEffect } from 'react';
-import axios from 'axios';
-import { Route,Link } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import '../../../css/LandingPage/LandingPage.css';
 import maintitle from '../../../assets/images/main_title.gif';
 import mainvideo from '../../../assets/images/main_background.mp4'
-import NavBar from '../Navbar/Navbar';
+import {ReactComponent as Logo } from '../../../assets/images/navbar/logo.svg';
 
 const LandingPage = () => {
     return(
         <div className="Mainpage">
-            <img className="main_title" src={maintitle} alt="logo"/>
+            <Logo className="main_title"/>
+            {/* <img className="main_title" src={Logo} alt="logo"/> */}
             <button className="start-btn"><Link to="/testintro">start</Link></button>
             <video className="back-video" autoplay muted>
                 <source src={mainvideo} type="video/mp4"/>
---
+++ client/src/components/views/LoadingPage/LoadingPage.js
@@ -0,0 +1,13 @@
+import React,{ useEffect, useState, useRef } from 'react';
+import {ReactComponent as LoadingSymbol} from '../../../assets/images/symbols/Loading_symbol.svg';
+
+const LoadingPage = () => {
+    return(
+        <div>
+            <LoadingSymbol />
+            
+        </div>
+    )
+}
+
+export default LoadingPage;
