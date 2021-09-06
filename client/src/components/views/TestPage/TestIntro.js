b6ac7f1c70932e79857824235a11b48a510ca848
Im Chaewon <lisa425@sookmyung.ac.kr>
2021-09-06 16:25:19 +0900

    style:testintro responsive

--- client/src/components/views/TestPage/TestIntro.js
+++ client/src/components/views/TestPage/TestIntro.js
@@ -7,13 +7,13 @@ import introNext from '../../../assets/images/introNext.svg'
 const StartPage = () => {
     return(
         <div className="start-page">
-            <h1 className="first">Can we</h1>
-            <div className="without">
-                <h1 className="second">live without</h1>
+            <h1 className="first">Can we live</h1>
+            <h1 className="second">without</h1>
+            <h1 className="third">digital technology?</h1>
+            <div className="intro-text">
                 <p className="start">디지털 없이 살아갈 수 없는 세상 속,<br/>당신은 얼마나 잘 적응하고 있을까요?<br/>당신의 데이터 사용 습관을 분석해보세요!</p>
                 <Link to="/test"><img src={introNext} alt="바로가기"/></Link>
             </div>
-            <h1 className="third">digital technology?</h1>
         </div>
         
     );
