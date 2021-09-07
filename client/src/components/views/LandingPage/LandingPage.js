11421d40ecc21084aae20eeca11a717e7a55a5e0
Im Chaewon <lisa425@sookmyung.ac.kr>
2021-09-07 13:37:52 +0900

    fix:video autoplay

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../../../css/LandingPage/LandingPage.css';
-import maintitle from '../../../assets/images/main_title.gif';
 import mainvideo from '../../../assets/images/main_background.mp4'
 import {ReactComponent as Logo } from '../../../assets/images/navbar/logo.svg';
 
@@ -11,9 +10,11 @@ const LandingPage = () => {
             <Logo className="main_title"/>
             {/* <img className="main_title" src={Logo} alt="logo"/> */}
             <button className="start-btn"><Link to="/testintro">start</Link></button>
-            <video className="back-video" autoplay muted>
-                <source src={mainvideo} type="video/mp4"/>
-            </video>
+            <div className="video-area">
+                <video className="back-video" autoPlay muted loop>
+                    <source src={mainvideo} type="video/mp4"/>
+                </video>
+            </div>
         </div> 
     );
 };
