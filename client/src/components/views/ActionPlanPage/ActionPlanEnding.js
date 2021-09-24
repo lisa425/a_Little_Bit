f149a740b9177a0c02ad0ab8ef7579d5e27b7032
Im Chaewon <lisa425@sookmyung.ac.kr>
2021-09-24 16:45:43 +0900

    fix:ending css

--- client/src/components/views/ActionPlanPage/ActionPlanEnding.js
+++ client/src/components/views/ActionPlanPage/ActionPlanEnding.js
@@ -1,7 +1,7 @@
 import React,{useState,useEffect} from 'react';
 import '../../../css/ActionPlanPage/ActionPlanEnding.css';
-import noGlitch from '../../../assets/images/actionplans/no-glitch.gif';
-import yesGlitch from '../../../assets/images/actionplans/yes-glitch.gif';
+import noGlitch from '../../../assets/images/actionplans/no-glitch.mp4';
+import yesGlitch from '../../../assets/images/actionplans/yes-glitch.mp4';
 import { Link } from "react-router-dom";
 import Axios from 'axios';
 
@@ -75,16 +75,14 @@ const ActionPlanEnding = () => {
 
             <section className={click? "glitch-motion" : "hide"}>
                 {Answer &&
-                    <img src={yesGlitch} alt="yes glitch"/>
-                    // <video autoPlay muted playsInline>
-                    //     <source src={yesGlitch} type="video/mp4"/>
-                    // </video>
+                    <video autoPlay muted playsInline>
+                        <source src={yesGlitch} type="video/mp4"/>
+                    </video>
                 }
                 {!Answer && 
-                    <img src={noGlitch} alt="no glitch"/>
-                    // <video autoPlay muted playsInline>
-                    //     <source src={noGlitch} type="video/mp4"/>
-                    // </video>
+                    <video autoPlay muted playsInline>
+                        <source src={noGlitch} type="video/mp4"/>
+                    </video>
                 }
             </section>
             {showEnding(Answer)}
